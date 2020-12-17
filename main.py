e92c5669f0a9e8d941a480099b59da536ae6f278
andre williamson <AndreWilliamsonMN@gmail.com>
2020-12-17 14:05:57 +0000

    done with code, used for range loop

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 #Write your code below this row 👇
-
+num = 0
+for num in range(0,101):
+  if num % 3 == 0  and num % 5 == 0:
+   print("fizzbuzz")
+  elif num % 3 == 0:
+   print("fizz")
+  elif num % 5 == 0:
+   print("buzz")
+  else:
+   print(num)      
